3dba92525ce09798a07cdd60c653870a9ee0b43c
rdarst <45427115+rdarst@users.noreply.github.com>
2020-05-25 19:32:59 -0400

    Update Dome9.tf

--- Dome9.tf
+++ Dome9.tf
@@ -223,8 +223,8 @@ resource "dome9_iplist" "iplist_1" {
         comment = "Net 10 dot 2"
           }
   items  {
-        ip = "10.4.0.0/16"
-        comment = "Net 10 dot 4"
+        ip = "10.88.0.0/16"
+        comment = "Net 10 dot 88"
           }
   items  {
         ip = "192.168.1.3/32"
