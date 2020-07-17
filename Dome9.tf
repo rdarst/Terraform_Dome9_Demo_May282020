1e2d5c7c75d2c107ad4981006c2d7b766cf35a4e
rdarst <45427115+rdarst@users.noreply.github.com>
2020-07-17 13:14:18 -0400

    Update net 10 dot 8

--- Dome9.tf
+++ Dome9.tf
@@ -231,8 +231,8 @@ resource "dome9_iplist" "iplist_1" {
         comment = "Net 10 dot 2"
           }
   items  {
-        ip = "10.7.0.0/16"
-        comment = "Net 10 dot 7"
+        ip = "10.8.0.0/16"
+        comment = "Net 10 dot 8"
           }
   items  {
         ip = "192.168.1.3/32"
