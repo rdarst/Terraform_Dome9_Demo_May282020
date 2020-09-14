05ae8b3d4b7ef0f1303de325e77a6dd6f13bce43
rdarst <45427115+rdarst@users.noreply.github.com>
2020-09-14 14:36:30 -0400

    Update Dome9.tf

--- Dome9.tf
+++ Dome9.tf
@@ -129,6 +129,14 @@ resource "dome9_cloudaccount_aws" "AWS_CPX2020" {
       new_group_behavior = "ReadOnly"
       region             = "me_south_1"
     } 
+    regions {
+      new_group_behavior = "ReadOnly"
+      region             = "af_south_1"
+    } 
+    regions {
+      new_group_behavior = "ReadOnly"
+      region             = "eu_south_1"
+    } 
   }
 }
 
